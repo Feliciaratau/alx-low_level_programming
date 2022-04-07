50981d2589c8287d622735162294a2933c437860
“Felicia <feliciaratau@gmail.com>
2022-04-07 21:41:45 +0200

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
+
 /**
- *main - Entry point 
- *Return: Always 0 (Success)
- */
-int main (void)
+* main -Entry point
+*
+* Description: prints "Programming is like builiding a multilingual puzzle"
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
 {
- puts ("\"Programming is like builidng a multilingual puzzle\n");
- return (0);
+puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
 }
