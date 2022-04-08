85be6c6b77bfb4afd7c78b44ab7cbb2312be8edf
“Felicia <feliciaratau@gmail.com>
2022-04-08 04:20:27 +0200

    update

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
 /**
-*
-* main - Entry point
-*
-* Return: Allways 0 (Success)
-*/
+ * main - print the size of various types
+ *
+ * Description: prints the size of various types on the computer it is compiled
+ * and run on
+ *
+ * Return: Allways 0 (Success)
+ */
 
 int main(void)
 {
