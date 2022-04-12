e2db00221d5524d2f2bc9d29725e34a31e2deb00
“Felicia <feliciaratau@gmail.com>
2022-04-12 21:21:25 +0200

    update

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* prints_alphabet_x10 -  prints 10 times the alphabet in lowercase,
+* prints_alphabet_x10 - prints 10 times the alphabet in lowercase,
 * followed by a new line
 * Return: void
 */
